481361b1043087f63398b85a2dab56bb7134fcd7
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-03-28 22:11:37 -0400

    Model setup and Some changes
    Modified Controllers and Implemented API Post Req

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,15 @@
+import User from "../models/User.js"
+import { StatusCodes } from 'http-status-codes'
 
-const register = async (req,res) => {
-        res.send("Register User")
+const register = async (req,res, next) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(StatusCodes.OK).json({user});
+    } catch (err) {
+        // res.status(500).json({msg : err.msg})
+        next(err);
+    }
+        
 }
 
 const login = async (req,res) => {
