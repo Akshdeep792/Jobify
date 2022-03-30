44f0d26b6c171698a4c54e43a93029e01104b195
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-03-30 17:23:54 -0400

    Front-Back-End

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,4 +1,21 @@
+import { useEffect } from 'react'
+
 const Dashboard = () => {
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('/api/v1');
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
     return <div>Dashboard</div>
 }
 
--- server.js
+++ server.js
@@ -20,7 +20,12 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
 
-    res.send('Welcome!')
+    res.json({msg:'Welcome!'})
+})
+
+app.get('/api/v1', (req, res) => {
+
+    res.json({msg:'API'})
 })
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', jobRouter)
