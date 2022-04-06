dcd130e908936434f8e93ca54703bd09c90cd90a
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-04-06 12:54:31 -0400

    Sidebar Complete

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,7 +1,18 @@
 import Wrapper from "../assets/wrappers/BigSidebar"
+import { useAppContext } from "../context/appContext"
+import NavLinks from "./Navlinks";
+import Logo from "./UI/Logo";
 const BigSidebar = () => {
+    const {showSidebar} = useAppContext();
     return <Wrapper>
-        <h4>Big Sidebar</h4>
+        <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+            <div className='content'>
+                <header>
+                    <Logo />
+                </header>
+                <NavLinks />
+            </div>
+        </div>
     </Wrapper>
 }
 
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,8 +1,29 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
-const SmallSidebar = () => {
-    return <Wrapper>
-        <h4>Small Sidebar</h4>
-    </Wrapper>
+
+import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
+import Logo from './UI/Logo'
+import NavLinks from './Navlinks'
+
+export const SmallSidebar = () => {
+    const { showSidebar, toggleSidebar } = useAppContext();
+    return (
+        <Wrapper>
+            <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+                <div className='content'>
+                    <button className='close-btn' onClick={toggleSidebar}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <NavLinks toggleSidebar={toggleSidebar}/>
+                </div>
+            </div>
+        </Wrapper>
+    )
 }
 
 export default SmallSidebar
