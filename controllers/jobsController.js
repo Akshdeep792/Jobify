c30c469a80d65f881a1dbb1e73bfe29428b87d68
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-04-14 15:53:30 -0400

    Search Feature Server

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -18,8 +18,45 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ job })
 }
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy: req.user.userId })
-    res.status(StatusCodes.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+    const getAllJobs = async (req, res) => {
+        const { search, status, jobType, sort } = req.query
+      
+        const queryObject = {
+          createdBy: req.user.userId,
+        }
+      
+        if(status !== 'all'){
+            queryObject.status = status
+        }
+        if(jobType !== 'all'){
+            queryObject.jobType = jobType
+        }
+        if(search){
+            queryObject.position = {$regex : search, $options: 'i'}
+        }
+        // NO AWAIT
+        let result = Job.find(queryObject)
+      
+        // chain sort conditions
+        if (sort === 'latest') {
+            result = result.sort('-createdAt')
+          }
+          if (sort === 'oldest') {
+            result = result.sort('createdAt')
+          }
+          if (sort === 'a-z') {
+            result = result.sort('position')
+          }
+          if (sort === 'z-a') {
+            result = result.sort('-position')
+          }
+      
+        const jobs = await result
+      
+        res
+          .status(StatusCodes.OK)
+          .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+      }
 }
 const updateJob = async (req, res) => {
     const { id: jobId } = req.params;
